9b7455a491fbcdcc64508aff5a871ebb8587e550
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-09-01 18:42:45 +0100

    Build page structure

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,23 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	mode: 'development',
+  mode: 'development',
   entry: './src/index.js',
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-		}),
-	],
-	devServer: {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
 		    static: './dist',
-	},
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-		clean: true,
-		publicPath: '/',
+    clean: true,
+    publicPath: '/',
   },
-	module: {
+  module: {
 		    rules: [
 	      {
 		        test: /\.css$/i,

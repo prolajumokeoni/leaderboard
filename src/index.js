abd0235a191a29a038fa65699edf5afa8dacd2c5
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-09-02 12:54:09 +0100

    Add display function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import './style.css';
+const scoreList = document.getElementById('scorelist')
+const user = document.getElementById('user')
+const score = document.getElementById('score')
+const submitButton = document.getElementById('submitButton')
+
+const scores = [
+	{
+		user : 'Victor',
+		score : 20
+	}
+]
+
+function displayList () {
+	scores.forEach(val => {
+	const li = document.createElement('li')
+		li.classList.add('list-group-item');
+				const text = `
+				<div class="d-flex justify-content-between align-items-start" id="${val.index}">
+					<span class="fw-bold">
+						${val.user}
+					</span>
+					<span class="fw-bold">
+					${val.score}
+					</span>
+				</div>`;
+				li.innerHTML = text
+				scoreList.appendChild(li)
+	})
+}
+
+displayList();

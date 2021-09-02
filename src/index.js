2acf25e17bf9887fc6d7099575538e3f14b2bc30
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-09-02 14:05:23 +0100

    Add submit button function

--- src/index.js
+++ src/index.js
@@ -1,32 +1,59 @@
-import './style.css';
-const scoreList = document.getElementById('scorelist')
-const user = document.getElementById('user')
-const score = document.getElementById('score')
-const submitButton = document.getElementById('submitButton')
+import "./style.css";
+const scoreList = document.getElementById("scorelist");
+const userName = document.getElementById("user");
+const scoreMark = document.getElementById("score");
+const submitButton = document.getElementById("submitButton");
+const refreshButton = document.getElementById("refreshButton");
 
-const scores = [
-	{
-		user : 'Victor',
-		score : 20
-	}
-]
-
-function displayList () {
-	scores.forEach(val => {
-	const li = document.createElement('li')
-		li.classList.add('list-group-item');
-				const text = `
-				<div class="d-flex justify-content-between align-items-start" id="${val.index}">
-					<span class="fw-bold">
-						${val.user}
-					</span>
-					<span class="fw-bold">
-					${val.score}
-					</span>
-				</div>`;
-				li.innerHTML = text
-				scoreList.appendChild(li)
-	})
+let scores = [];
+let apikey = "jkEzbR6fNifTU9BFqJSn";
+async function displayList() {
+  const response = await fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apikey}/scores/`
+  );
+  scores = await response.json();
+  if (scores.result.length) {
+    scores.result.forEach((val) => {
+      const li = document.createElement("li");
+      li.classList.add("list-group-item");
+      const text = `
+						<div class="d-flex justify-content-between align-items-start" id="${val.index}">
+							<span class="fw-bold">
+								${val.user}
+							</span>
+							<span class="fw-bold">
+							${val.score}
+							</span>
+						</div>`;
+      li.innerHTML = text;
+      scoreList.appendChild(li);
+    });
+  }
 }
 
 displayList();
+
+submitButton.addEventListener("click", async (e) => {
+  e.preventDefault();
+  let scoreData = {
+    user: userName.value,
+    score: scoreMark.value,
+  };
+
+  const addedData = await fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apikey}/scores/`,
+    {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(scoreData),
+    }
+  );
+  if (addedData) {
+    scoreList.innerHTML = "";
+    await displayList();
+    userName.value = "";
+    scoreMark.value = "";
+  }
+});
+
+

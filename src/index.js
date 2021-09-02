68765685e496e93f21dc60e0ca7c75f54c66698a
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-09-02 15:28:20 +0100

    Fix errors

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
-import "./style.css";
-const scoreList = document.getElementById("scorelist");
-const userName = document.getElementById("user");
-const scoreMark = document.getElementById("score");
-const submitButton = document.getElementById("submitButton");
-const refreshButton = document.getElementById("refreshButton");
+import './style.css';
+
+const scoreList = document.getElementById('scorelist');
+const userName = document.getElementById('user');
+const scoreMark = document.getElementById('score');
+const submitButton = document.getElementById('submitButton');
+const refreshButton = document.getElementById('refreshButton');
 
 let scores = [];
-let apikey = "jkEzbR6fNifTU9BFqJSn";
+const apikey = 'jkEzbR6fNifTU9BFqJSn';
 async function displayList() {
   const response = await fetch(
-    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apikey}/scores/`
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apikey}/scores/`,
   );
   scores = await response.json();
   if (scores.result.length) {
     scores.result.forEach((val) => {
-      const li = document.createElement("li");
-      li.classList.add("list-group-item");
+      const li = document.createElement('li');
+      li.classList.add('list-group-item');
       const text = `
-						<div class="d-flex justify-content-between align-items-start" id="${val.index}">
-							<span class="fw-bold">
-								${val.user}
-							</span>
-							<span class="fw-bold">
-							${val.score}
-							</span>
-						</div>`;
+      <div class="d-flex justify-content-between align-items-start" id="${val.index}">
+        <span class="fw-bold">
+          ${val.user}
+        </span>
+        <span class="fw-bold">
+        ${val.score}
+        </span>
+      </div>`;
       li.innerHTML = text;
       scoreList.appendChild(li);
     });
@@ -33,9 +34,9 @@ async function displayList() {
 
 displayList();
 
-submitButton.addEventListener("click", async (e) => {
+submitButton.addEventListener('click', async (e) => {
   e.preventDefault();
-  let scoreData = {
+  const scoreData = {
     user: userName.value,
     score: scoreMark.value,
   };
@@ -43,21 +44,20 @@ submitButton.addEventListener("click", async (e) => {
   const addedData = await fetch(
     `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apikey}/scores/`,
     {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(scoreData),
-    }
+    },
   );
   if (addedData) {
-    scoreList.innerHTML = "";
+    scoreList.innerHTML = '';
     await displayList();
-    userName.value = "";
-    scoreMark.value = "";
+    userName.value = '';
+    scoreMark.value = '';
   }
 });
 
-
-refreshButton.addEventListener("click", (e) => {
-  scoreList.innerHTML = "";
+refreshButton.addEventListener('click', () => {
+  scoreList.innerHTML = '';
   displayList();
 });
